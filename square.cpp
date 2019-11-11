53131cd878eacea375c8f781046139ca73aa0772
sakshi991 <57613695+sakshi991@users.noreply.github.com>
2019-11-12 00:40:17 +0530

    Create square.cpp

---
+++ square.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+    clrscr();
+    int i=1,sqr;
+    while(i>0)
+    {
+        sqr =i*i;
+        cout<<"Squar root of "<<i<<" is "<<sqr<<endl;
+        i++
+    }
+    getch();
+}
